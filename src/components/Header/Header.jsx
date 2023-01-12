3d141b69a5f86beb1b4e0a256da1f70e36fcea99
ManuANGOT <manu.angot@hotmail.fr>
2023-01-12 16:49:27 +0100

    ajout du dossier fil rouge

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+import { useState } from "react";
+
+function Header(){
+
+    
+}
+
+export default Header;
---
+++ src/components/NavBar/Nav.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Nav.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+
+function Nav() {
+
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+  
+  const handleClick = () => {
+    console.log(toggleMenu);
+    toggleMenu ? setToggleMenu(false) : setToggleMenu(true);
+  };
+  
+
+  return (
+    <div>
+      <button className="nav_burger" onClick={handleClick}>
+        {/* <i class="bi bi-list"></i> */}
+      
+      </button>
+      <img
+        src=""
+        className="nav_logo"
+        alt="Logo"
+        width="20%"
+      />
+      <nav className="nav_links">
+        <a href="/" className="nav__link">
+          Menu
+        </a>
+
+        <a href="/" className="nav__link">
+          S'inscrire
+        </a>
+
+        <a href="/" className="nav__link">
+         Se connecter
+        </a>
+      </nav>
+      
+    </div>
+  );
+}
+
+export default Nav;
