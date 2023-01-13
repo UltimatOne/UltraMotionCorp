5bfd9da4e96499794bb8a6c35cf3c91dd840b9c7
eddy debuisson <eddydebuisson@gmail.com>
2023-01-13 13:58:27 +0100

    feat: début du Footer

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import Footer from './composants/footer/footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+    {/* <App /> */}
+    <Footer></Footer>
     </BrowserRouter>
   </React.StrictMode>
 );
