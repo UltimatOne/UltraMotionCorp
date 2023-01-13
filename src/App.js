84e92c24424ef08848044ae670d024fcc1da65bf
ManuANGOT <manu.angot@hotmail.fr>
2023-01-13 10:50:15 +0100

    ajout composant NavBar, boutons fonctionnels, css à compléter

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import Nav from './components/NavBar/Nav';
 
 
+
 function App() {
   return (
     <div className="App">
-      <p>projet UMC</p>
      {/* navbar */}
      <Nav/>
 
--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -1,49 +1,41 @@
 import React from "react";
 import "./Nav.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
+/* import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Outlet
+} from 'react-router-dom'; */
 
 function Nav() {
-
-
   const [toggleMenu, setToggleMenu] = useState(false);
-  
+
   const handleClick = () => {
     console.log(toggleMenu);
     toggleMenu ? setToggleMenu(false) : setToggleMenu(true);
   };
-  
 
   return (
-    <div>
+    <div className="banner">
       <button className="nav_burger" onClick={handleClick}>
         {/* <i class="bi bi-list"></i> */}
-      
       </button>
-      <img
-        src=""
-        className="nav_logo"
-        alt="Logo"
-        width="20%"
-      />
-      <nav className="nav_links">
-        <a href="/" className="nav__link">
-          <button>
-          Menu
-          </button>
-        </a>
+      <div>
+        <img src=".src/img/logoExemple.jpg" alt="LogoUMC" />
+      </div>
+      <span>
+        <nav className="nav_links">
+          <a href="/" className="nav__link">
+            <button className="btnInscription">S'inscrire</button>
+          </a>
 
-        <a href="/" className="nav__link">
-          <button>
-          S'inscrire
-          </button>
-        </a>
-
-        <a href="/" className="nav__link">
-         Se connecter
-        </a>
-      </nav>
-      
+          <a href="/" className="nav__link">
+            <button className="btnConnection">Se connecter</button>
+          </a>
+        </nav>
+      </span>
     </div>
   );
 }
