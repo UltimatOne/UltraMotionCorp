9cb79e8fcb338ea3bc160ac88d7a53ccfb11d3d2
ManuANGOT <manu.angot@hotmail.fr>
2023-01-13 11:37:44 +0100

    ajout du composant barre de recherche

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Accueil from './composants/pages/accueil/accueil';
+import SearchBar from './composants/searchBar/searchBar';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
       <Route path="/" element={<Accueil />}/>
       </Routes>
+      <SearchBar/>
     </div>
     
   );
