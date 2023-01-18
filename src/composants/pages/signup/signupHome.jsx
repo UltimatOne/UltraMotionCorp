173beee4f86c16cd050135983ff1a4e2f75ad858
eddy debuisson <eddydebuisson@gmail.com>
2023-01-18 16:57:28 +0100

    Feat: Début de la page Home SignUp :sparkles:

--- src/composants/pages/signup/signupHome.jsx
+++ src/composants/pages/signup/signupHome.jsx
@@ -4,7 +4,13 @@ export default function SignUpHome(){
             <div className="container">
                 <div className="top-utilisateur">Utilisateur</div>
                 <p>Client à la recherche de service</p>
-                
+                <p>Ce compte Vous permettra de : </p>
+                <ul>
+                <li>Recherche de services</li>
+                <li>Commander un Service</li>
+                <li>Obtenir de l'aide concernant vos services commandés</li>
+                </ul>
+                <button>Crée compte</button>
             </div>
         </div>
 )}
