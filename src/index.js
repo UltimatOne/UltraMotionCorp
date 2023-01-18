79da5be4e32e2004cff55849cf2554d927a2036d
Jean-Jacques Goddet <jj.goddet@icloud.com>
2023-01-18 16:48:39 +0100

    suppression ligne import footer dans index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import Footer from './composants/footer/footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
