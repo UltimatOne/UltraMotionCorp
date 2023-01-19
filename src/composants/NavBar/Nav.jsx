eb5c9eaabb7512697bc6cc0ef4731d5cdea21697
Miloud Benbahlouli <miloudbenbahlouli@gmail.com>
2023-01-19 16:52:45 +0100

    feat: commentaires fichier navBar

--- src/composants/NavBar/Nav.jsx
+++ src/composants/NavBar/Nav.jsx
@@ -1,3 +1,4 @@
+//  Imports nécessaires
 import React from "react";
 import "./Nav.css";
 import logo from "../../medias/images/logoUMC.svg";
@@ -8,19 +9,26 @@ function Nav() {
   return (
     <div className="banner">
       <div className="container">
+        {/* Logo svg UMC importé */}
         <div>
           <img src={logo} alt="LogoUMC" />
         </div>
         <span>
           <nav className="nav_links">
+              {/* Balise Bouton, props Children= S'inscrire, 
+              props ClassName = btnInscription
+              props type=button + lien formulaire inscription*/}
             <a href="/formulaire-utilisateur" className="nav__link">
               <Button className="btnInscription" type="button">
                 S'inscrire
               </Button>
             </a>
 
+            {/* Balise Bouton, props Children= Se Connecter, 
+              props ClassName = btnConnexion
+              props type=button*/}
             <a href="/" className="nav__link">
-              <Button className="btnConnexion">Se Connecter</Button>
+              <Button className="btnConnexion" type="button">Se Connecter</Button>
             </a>
           </nav>
         </span>
