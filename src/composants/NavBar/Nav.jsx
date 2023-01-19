630017ef5a856859dc524ff2df731d7727ee8eef
Miloud Benbahlouli <miloudbenbahlouli@gmail.com>
2023-01-19 16:29:03 +0100

    feat: importation navBar dans page accueil

--- src/composants/NavBar/Nav.jsx
+++ src/composants/NavBar/Nav.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 import "./Nav.css";
-// import { useState } from "react";
 import logo from "../../medias/images/logoUMC.svg";
 import Button from "../button/button";
-/* import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  Outlet
-} from 'react-router-dom'; */
+
 
 function Nav() {
   return (
