110c78322e5a3df592d43f4f9fec61a60a02aeda
ManuANGOT <manu.angot@hotmail.fr>
2023-01-20 11:04:56 +0100

    ajout du composant Touvezo.. le bon  Artisan

--- src/composants/HomePage/Trouvez/trouvez.jsx
+++ src/composants/HomePage/Trouvez/trouvez.jsx
@@ -3,34 +3,37 @@ import React from "react";
 const Trouvez = () => {
   return (
     <div className="Trouvez">
-      
-        <h1> Trouvez le bon artisan ou la bonne entreprise</h1>
-        <h1> parmi nos professionnels</h1>
-       
-        <div>
-          <h1>1</h1>
-          <h3>Recherchez</h3>
-          <img src="./images/logoRecherchez.jpeg" alt="" />
-        </div>
+      <h1> Trouvez le bon artisan ou la bonne entreprise</h1>
+      <h1> parmi nos professionnels</h1>
 
-        <div className="Choisissez">
-          <h1>2</h1>
-          <h3>Choisissez votre prestataire</h3>
-          <img src="./images/logoChoisissez.jpeg" alt="" />
-        </div>
+      <div className="Trouvez1">
+        <img src="./images/logoTrouvez1.svg" alt="logoTrouvez1" />
+        <h3>Profitez d'offre de</h3>
+        <h3>menuiserie abordables</h3>
+        <h3>et sur-mesure</h3>
+      </div>
 
-        <div className="Reservez">
-          <h1>3</h1>
-          <h3>Réservez et payez</h3>
-          <img src="./images/logoReservez.jpeg" alt="" />
-        </div>
+      <div className="Trouvez2">
+        <img src="./images/logoTrouvez2.svg" alt="logoTrouvez2" />
+        <h3>Offres abordables</h3>
+        <h3>et demandes de</h3>
+        <h3>devis gratuits</h3>
+      </div>
 
-        <div className="Terminé">
-          <h1>4</h1>
-          <h3>Vos travaux sont terminés !</h3>
-          <img src="./images/logoTermine.jpeg" alt="" />
-        </div>
-      
+      <div className="Trouvez3">
+        <img src="./images/logoTrouvez3.svg" alt="logoTrouvez3" />
+        <h3>Faites appel à</h3>
+        <h3>des prestataires</h3>
+        <h3>de qualité pour réparer</h3>
+        <h3>vos équipements</h3>
+      </div>
+
+      <div className="Trouvez4">
+        <img src="./images/logoTrouvez4.svg" alt="logoTrouvez4" />
+        <h3>Ultra Motion Corp</h3>
+        <h3>vous accompagne</h3>
+        <h3>pour vos travaux</h3>
+      </div>
     </div>
   );
 };
