79d02917e678bc28a73188d86348415c26a5245b
Jean-Jacques Goddet <jj.goddet@icloud.com>
2023-01-23 11:11:24 +0100

    ajout barre de recherche + correction css formulaire utilisateur

--- src/composants/barreDeRecherche/BarreDeRecherche.jsx
+++ src/composants/barreDeRecherche/BarreDeRecherche.jsx
@@ -7,7 +7,6 @@ const BarreDeRecherche = ({ placeholder }) => {
   const [datas, setDatas] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
-
   useEffect(() => {
     fetch("http://localhost:8000/services")
       .then((response) => response.json())
@@ -17,33 +16,39 @@ const BarreDeRecherche = ({ placeholder }) => {
   const handleSearchTerm = (event) => {
     console.log(event.target.value);
     const value = event.target.value;
-    setSearchTerm(value)
+    setSearchTerm(value);
   };
   console.log();
-  
+
   return (
     <>
       <div className="recherche">
         <div className="rechercheContainer">
           <div className="inputRecherche">
-            <input type="text" placeholder={placeholder} onChange={handleSearchTerm}/>
+            <input
+              type="text"
+              placeholder={placeholder}
+              onChange={handleSearchTerm}
+            />
             <div className="iconRecherche">
-              <SearchIcon/>
+              <SearchIcon />
             </div>
           </div>
           {searchTerm.length !== 0 && (
-          <div className="resultats_recherche">
-            {datas.filter((datasFiltered)=> {
-              return datasFiltered.titre.includes(searchTerm)
-            }).map((datasFiltered, key) => {
-              return (
-                <a className="serviceItem" href="">
-                  <p key={datasFiltered.id}>{datasFiltered.titre}</p>
-                </a>
-              );
-            })}
-          </div>
-          )};
+            <div className="resultats_recherche">
+              {datas
+                .filter((datasFiltered) => {
+                  return datasFiltered.titre.includes(searchTerm);
+                })
+                .map((datasFiltered, key) => {
+                  return (
+                    <a className="serviceItem" href="">
+                      <p key={datasFiltered.id}>{datasFiltered.titre}</p>
+                    </a>
+                  );
+                })}
+            </div>
+          )}
         </div>
       </div>
     </>
