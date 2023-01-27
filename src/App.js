2d2d7263f8b8aba7a200b9ceacea41067b90364d
Jean-Jacques Goddet <jj.goddet@icloud.com>
2023-01-27 16:52:05 +0100

    ajout route /connexion dans app.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
       <Route path="/formulaire-client" element={<FormulaireClient props="Utilisateur" />}/>
       <Route path="/formulaire-entreprise" element={<FormulaireEntreprise props="Prestataire"/>}/>
       <Route path='/DescriptionService' element={<DescriptionService/>}/>
+      <Route path='/connexion' element={<Login/>}/>
       </Routes>
       <Footer></Footer>
     </div>
