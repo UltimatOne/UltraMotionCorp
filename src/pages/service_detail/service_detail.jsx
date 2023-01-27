6e3291b6eac9804849ff2fb70ca28808885df508
eddy debuisson <eddydebuisson@gmail.com>
2023-01-27 13:48:42 +0100

    feat: page en construction :construction:

--- src/pages/service_detail/service_detail.jsx
+++ src/pages/service_detail/service_detail.jsx
@@ -1,16 +1,21 @@
-import { Carousel } from "react-responsive-carousel"
 import "./service_detail.css"
+import img1 from "../../medias/images/1.jpg"
 
 export default function ServiceDetail(){
+    
+    const prestataire = {}
+    
     return (
             <div className="service_detail">
-                <Carousel></Carousel>
+                <div className="container-top">
+                <img src={img1} alt="" />
                 <div className="descriptif_prestataire">
                         <img src="" alt="" />
                         <p>Nom prestataire</p>
                         <p>Descriptif du prestataire:</p>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur tempore expedita quo neque quia eum enim aspernatur odio, ullam magni repellat assumenda aperiam soluta dolore nisi nemo qui modi recusandae!</p>
                         <button> AJOUTER AU PANIER</button>
+                </div>
                 </div>
                 <div className="descriptif_service">
                     <h2>Exemple de titre prestation</h2>
