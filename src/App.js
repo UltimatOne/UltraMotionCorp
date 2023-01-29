0365f236a1c1fb9388160f2582a19be14cb66049
Eddydebuisson <eddydebuisson@gmail.com>
2023-01-29 14:34:19 +0100

    feat: Possibilité de se connecter et changement de la navbar en fonction de si on est connecter ou non :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Footer from './composants/footer/footer';
 import FormulaireClient from './composants/formulaires/formulaireClient';
@@ -11,8 +14,10 @@ import SignUpHome from './pages/signup/signupHome';
 
 
 function App() {
+
   return (
     <div className="App">
+      <ToastContainer/>
       <Routes>
       <Route exact path="/" element={<Accueil />}/>
       <Route path='/home-signup' element={<SignUpHome/>} />
