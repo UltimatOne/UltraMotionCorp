573191b462526cea93cde9455cccbdfe18bb7aae
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2023-01-29 19:58:22 +0100

    modification du carroussel pour liaisons avec services fetch

--- src/composants/carrousel/carrousel.js
+++ src/composants/carrousel/carrousel.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "./carrousel.css";
-import image1 from "./../../medias/images/1.jpg"
-import image2 from "./../../medias/images/2.jpg"
-import image3 from "./../../medias/images/3.jpg"
+// import "./carrousel.css";
+import { useParams } from "react-router-dom";
+import dataService from "../services/dataService";
 
 const Carrousel = () => {
+  const [serviceImage, setServiceImage] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    if (id) {
+      dataService.getService(+id).then((data) => setServiceImage(data));
+    }
+  }, [id]);
+
   return (
     <Carousel className="carrousel" autoPlay="true" infiniteLoop="true" interval={4000}>
       <div>
-        <img src={image1} alt="image1"></img>
+        <img src={serviceImage.image1} alt="image1"></img>
       </div>
       <div>
-      <img src={image2} alt="image2"></img>
+      <img src={serviceImage.image2} alt="image2"></img>
       </div>
       <div>
-      <img src={image3} alt="image3"></img>
+      <img src={serviceImage.image3} alt="image3"></img>
       </div>
     </Carousel>
   );
